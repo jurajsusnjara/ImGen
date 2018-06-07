261bc134c2bd4829d4d60ac702a4aef152f9a1d7
Juraj <jurajsusnjara@gmail.com>
2018-06-08 00:52:13 +0200

    Caption image features.

--- ImGen/caption.py
+++ ImGen/caption.py
@@ -1,7 +1,31 @@
 import os
+import pickle
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications import vgg19
+from keras.applications.imagenet_utils import decode_predictions
+from keras import backend as K
+import numpy as np
 
 
-class Mapping():
+def load_pickle(fname):
+    with open(fname + '.pickle', 'rb') as handle:
+        a = pickle.load(handle)
+    return a
+
+
+def save_pickle(fname, obj):
+    with open(fname + '.pickle', 'wb') as handle:
+        pickle.dump(obj, handle)
+
+
+def get_id_from(fname):
+    start_idx = 4
+    end_idx = fname.find('.')
+    return int(fname[start_idx:end_idx])
+
+
+class Mapping:
     def __init__(self):
         self.mapping = {}
 
@@ -12,12 +36,6 @@ class Mapping():
             for line in f:
                 tags.append(line.replace('\n', ''))
         return tags
-
-    @staticmethod
-    def get_id_from(fname):
-        start_idx = 4
-        end_idx = fname.find('.')
-        return int(fname[start_idx:end_idx])
 
     @staticmethod
     def get_ids_from(fname):
@@ -42,7 +60,7 @@ class Mapping():
         for fname in os.listdir(tag_dir):
             full_path = tag_dir + '/' + fname
             tags = self.get_tags_from(full_path)
-            id = self.get_id_from(fname)
+            id = get_id_from(fname)
             self.append2dict(id, tags)
         return self.mapping
 
@@ -53,11 +71,50 @@ class Mapping():
             self.append_from_ann(full_path, ann)
         return self.mapping
 
+    def save_pickle(self, f):
+        with open(f + '.pickle', 'wb') as handle:
+            pickle.dump(self.mapping, handle)
 
-tags_dir = '/home/juraj/Desktop/flickr_data/mirflickr/meta/tags'
-ann_dir = '/home/juraj/Desktop/flickr_data/mirflickr25k_annotations_v080'
-mapping = Mapping()
-mapping.extract_annotations(ann_dir)
-mapping.extract_id2tags_mapping(tags_dir)
 
-# TODO duplikati
+class ImageFeatures:
+    def __init__(self):
+        self.model = vgg19.VGG19(weights='imagenet')
+        self.get_features = K.function([self.model.layers[0].input],
+                              [self.model.layers[-2].output])
+        self.img_feats = {}
+
+    def img2feats(self, fname):
+        original = load_img(fname, target_size=(224, 224))
+        numpy_image = img_to_array(original)
+        image_batch = np.expand_dims(numpy_image, axis=0)
+        processed_image = vgg19.preprocess_input(image_batch.copy())
+
+        return self.get_features([processed_image])[0]
+
+    def imgs2feats(self, dir):
+        c = 0
+        for fname in os.listdir(dir):
+            c += 1
+            print(c, '/ 25000')
+            full_path = dir + '/' + fname
+            id = get_id_from(fname)
+            self.img_feats[id] = self.img2feats(full_path)
+        return self.img_feats
+
+
+def generate_mapping():
+    tags_dir = '/home/juraj/Desktop/flickr_data/mirflickr/meta/tags'
+    ann_dir = '/home/juraj/Desktop/flickr_data/mirflickr25k_annotations_v080'
+    mapping = Mapping()
+    mapping.extract_annotations(ann_dir)
+    mapping.extract_id2tags_mapping(tags_dir)
+    mapping.save_pickle('mapping')
+
+
+if __name__ == '__main__':
+    dir = 'imgs'
+    save_path = 'img_features'
+    img = ImageFeatures()
+    res = img.imgs2feats(dir)
+    print('Saving results')
+    save_pickle(save_path, res)
